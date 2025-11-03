ef6033cd91c0a2d0b38d56dfd0d664da16691cfd
XuanDuNe <nguyenxuandu0401@gmail.com>
2025-11-03 13:39:27 +0700

    Change name and delete service for Diagram

--- services/storage-service/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.sql import func
-from database import Base
-
-class File(Base):
-    __tablename__ = "files"
-    filename = Column(String, primary_key=True, index=True)
-    content_type = Column(String)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
